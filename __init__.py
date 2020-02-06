7e39517769d93c357a994c05ba3ca3856cf37ebb
Andre Marianiello <andre.marianiello@gmail.com>
2020-02-05 20:46:09 -0500

    Add basic initiative order functionality

--- __init__.py
+++ __init__.py
@@ -4,14 +4,50 @@ from mycroft import MycroftSkill, intent_file_handler
 class InitiativeTracker(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
+        self.initiative_order = dict()
 
-    @intent_file_handler('tracker.initiative.intent')
-    def handle_tracker_initiative(self, message):
+    @intent_file_handler('add.to.initiative.intent')
+    def handle_add_character(self, message):
         character = message.data.get('character')
+        initiative = message.data.get('initiative')
+        self.initiative_order[character] = int(initiative)
+        self.log.debug("initiative", self.initiative_order)
+        self.log.debug(self.initiative_order)
 
-        self.speak_dialog('tracker.initiative', data={
+        self.speak_dialog('added.to.initiative', data={
+            'character': character,
+            'initiative': initiative
+        })
+
+    @intent_file_handler('remove.from.initiative.intent')
+    def handle_remove_character(self, message):
+        character = message.data.get('character')
+        del self.initiative_order[character]
+        self.log.debug("initiative", self.initiative_order)
+
+        self.speak_dialog('removed.from.initiative', data={
             'character': character
         })
+
+    @intent_file_handler('what.is.initiative.intent')
+    def handle_initiative_query(self, message):
+        character = message.data.get('character')
+        if character is None:
+            if len(self.initiative_order) == 0:
+                self.speak_dialog('empty.initiative.order')
+            else:
+                for character, initiative in self.initiative_order.items():
+                    self.speak_dialog('character.has.initiative', data={
+                        'character': character,
+                        'initiative': initiative
+                    })
+        else:
+            if character.endswith("'s"):
+                character = character[:-2]
+            self.speak_dialog('character.has.initiative', data={
+                'character': character,
+                'initiative': self.initiative_order[character]
+            })
 
 
 def create_skill():
